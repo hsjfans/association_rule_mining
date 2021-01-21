73bc0a9f0fd591ce5e0938bdf2df3c5118636628
hsj <hsjfans.scholar@gmail.com>
2021-01-21 14:58:32 +0800

    add rules

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 主函数
 """
 from logger import logger
-from model import generate_c_k_plus_1, generate_l_k
+from model import generate_c_k_plus_1, generate_l_k, count_freq, find_frequent_patterns, calculate_association_rules, generate_association_rules
 from data_loader import load_data
 import argparse
 import time
@@ -11,9 +11,10 @@ import time
 def dummy_apriori(itemset, c_ks, support, num):
     all_lks = []
     all_sps = []
+    counter = count_freq(itemset)
 
     def apriori(itemset, c_k, support):
-        l_ks, sp_s, itemset = generate_l_k(itemset, c_k, support, num)
+        l_ks, sp_s, itemset = generate_l_k(itemset, c_k, support, num, counter)
         all_lks.extend(l_ks)
         all_sps.extend(sp_s)
         if len(l_ks) > 1:
@@ -26,20 +27,52 @@ def dummy_apriori(itemset, c_ks, support, num):
 
 
 def advanced_apriori1(itemset, c_ks, support, num):
-    pass
+    all_lks = []
+    all_sps = []
+    counter = count_freq(itemset)
+
+    def apriori(itemset, c_k, support):
+        l_ks, sp_s, itemset = generate_l_k(itemset, c_k, support, num, counter)
+        all_lks.extend(l_ks)
+        all_sps.extend(sp_s)
+        if len(l_ks) > 1:
+            ck_plus_1s = generate_c_k_plus_1(l_ks)
+            # 类间排序
+            ck_plus_1s = sorted(ck_plus_1s)
+            apriori(itemset, ck_plus_1s, support)
+
+    apriori(itemset, c_ks, support)
+
+    return all_lks, all_sps
 
 
 def advanced_apriori3(itemset, c_ks, support, num):
-    pass
+    all_lks = []
+    all_sps = []
+    counter = count_freq(itemset)
+
+    def apriori(itemset, c_k, support):
+        l_ks, sp_s, itemset = generate_l_k(
+            itemset, c_k, support, num, counter, trick='trick_3')
+        all_lks.extend(l_ks)
+        all_sps.extend(sp_s)
+        if len(l_ks) > 1 and len(itemset) > 0:
+            ck_plus_1s = generate_c_k_plus_1(l_ks)
+            apriori(itemset, ck_plus_1s, support)
+
+    apriori(itemset, c_ks, support)
+
+    return all_lks, all_sps
 
 
 def advanced_apriori2(itemset, c_ks, support, num):
     all_lks = []
     all_sps = []
+    counter = count_freq(itemset)
 
     def apriori(itemset, c_k, support):
         l_ks, sp_s, itemset = generate_l_k(
-            itemset, c_k, support, num, trick=True)
+            itemset, c_k, support, num, counter, trick='trick_2')
         all_lks.extend(l_ks)
         all_sps.extend(sp_s)
         if len(l_ks) > 1 and len(itemset) > 0:
@@ -65,10 +98,14 @@ def main(args):
         all_lks, all_sps = advanced_apriori2(itemset, c_ks, support, num)
     elif args.ty == 'apriori3':
         all_lks, all_sps = advanced_apriori3(itemset, c_ks, support, num)
+    elif args.ty == 'fpgrowth':
+        all_lks = find_frequent_patterns(itemset, support)
+        all_sps = []
     else:
         raise(
             f"Not support algorithm {args.ty}, Please choose from [`dummy`,`apriori1`,`apriori2`,`apriori3`]")
     interval = time.time() - start
+    logger.info(f"finish algorithm with {interval}")
     return all_lks, all_sps, interval
 
 
@@ -80,22 +117,44 @@ def get_k_set(all_lks, k=3):
     return l_set
 
 
+def get_association_rules(all_lks, all_sps, confidence, model='fp'):
+    if model == 'fpgrowth':
+        return generate_association_rules(all_lks, confidence)
+    elif model == 'apriori':
+        return calculate_association_rules(all_lks, all_sps, confidence)
+    else:
+        raise(
+            f"Not support algorithm {model}, Please choose from [`fp`,`apriori`]")
+
+
 def opt():
     parser = argparse.ArgumentParser(
         description='The association rule mining')
     parser.add_argument('-support', type=float,
                         default=0.01, help='The support rate')
+    parser.add_argument('-confidence', type=float,
+                        default=0.5, help='The confidence rate')
+    parser.add_argument('-k', type=int, default=3,
+                        help='The size of k_item_freq')
     parser.add_argument('-path', type=str,
                         default='./data/Groceries.csv',
                         help='The path of dataset')
     parser.add_argument('-ty', type=str,
                         default='dummy',
-                        help='The type of algorithm, must be one of [`dummy`,`apriori1`,`apriori2`,`apriori3`]')
+                        help='The type of algorithm, must be one of [`dummy`,`apriori1`,`apriori2`,`apriori3`,`fp`]')
     args = parser.parse_args()
+    if args.ty == 'fpgrowth':
+        args.model = 'fpgrowth'
+    else:
+        args.model = 'apriori'
     return args
 
 
 if __name__ == "__main__":
-    all_lks, all_sps, interval = main(opt())
-    k_set = get_k_set(all_lks, k=3)
-    print(k_set)
+    args = opt()
+    all_lks, all_sps, interval = main(args)
+    k_set = get_k_set(all_lks, k=args.k)
+    print(len(all_lks), len(k_set), k_set)
+    rules = get_association_rules(
+        all_lks, all_sps, args.confidence, model=args.model)
+    print(len(rules), rules)
--- model.py
+++ model.py
@@ -3,6 +3,46 @@
 """
 from logger import logger
 from tqdm import tqdm
+import copy
+import pyfpgrowth
+from itertools import combinations
+
+
+def trick_2(all_record_nums, itemset, k):
+    """
+    trick_2: 减小事务表(数据记录表)规模。
+
+    """
+    dataset = []
+    for record_num, item in zip(all_record_nums, itemset):
+        if record_num > k:
+            dataset.append(item)
+    logger.info(f"before: {len(itemset)}, after: {len(dataset)}")
+    return dataset
+
+
+def trick_3(counter, k, itemset):
+    """
+    trick_3 : 减少事务表中元组的项。
+
+    Parameters
+    ----------
+    counter : [dict]
+        每一个元素的词频
+    k : [int]
+        候选集元素的个数
+    itemset : [list]
+        事务表
+    """
+    dataset = []
+    for item in itemset:
+        elements = copy.deepcopy(item)
+        for element in item.keys():
+            if counter[element] <= k:
+                del elements[element]
+        if len(elements) > 0:
+            dataset.append(elements)
+    return dataset
 
 
 def support_rate(itemset, c_k, num):
@@ -24,11 +64,11 @@ def support_rate(itemset, c_k, num):
     return count / num, record_nums
 
 
-def confidence_rate(itemset, A, B):
-    """ confidence rate
-    置信度
-    """
-    pass
+# def confidence_rate(itemset, A, B):
+#     """ confidence rate
+#     置信度
+#     """
+#     pass
 
 
 def generate_c_k_plus_1(l_ks):
@@ -40,13 +80,15 @@ def generate_c_k_plus_1(l_ks):
                   desc=f'Generate the c_k_plus_1 {len(l_ks[0])+1} dataset', mininterval=0.1):
         for j in range(i + 1, len(l_ks)):
             # 去重复
-            c_k_plus_1s[tuple(l_ks[i].union(l_ks[j]))] = 1
+            item = sorted(l_ks[i].union(l_ks[j]))
+            c_k_plus_1s[tuple(item)] = 1
     logger.info(f'get the c_k+1 {len(l_ks[0])+1}:{len(c_k_plus_1s)}')
     return [set(c_k_1) for c_k_1 in list(c_k_plus_1s.keys())]
 
 
-def generate_l_k(itemset, c_ks, support, num, trick=False):
+def generate_l_k(itemset, c_ks, support, num, counter, trick=None):
     """ Generate l_k from c_k
+    trick: trick or not
     """
     l_k = []
     sp_s = []
@@ -57,20 +99,87 @@ def generate_l_k(itemset, c_ks, support, num, trick=False):
         sp, record_nums = support_rate(itemset, c_k, num)
         for i in range(len(record_nums)):
             all_record_nums[i] += record_nums[i]
-        if sp >= support:
+        if sp > support:
             l_k.append(c_k)
             sp_s.append(sp)
-        # else:
-            # logger.info(
-            #     f'c_k: {c_k}\'s support:{sp} lower of the support:{support}, filter it')
 
     # Advanced aprorio 2 filter
-    if trick:
-        dataset = []
-        for record_num, item in zip(all_record_nums, itemset):
-            if record_num > k:
-                dataset.append(item)
-        logger.info(f"before: {len(itemset)}, after: {len(dataset)}")
-        itemset = dataset
+    if trick is None:
+        logger.info('dummy algorithm')
+    elif trick == 'trick_2':
+        itemset = trick_2(all_record_nums, itemset, k)
+    elif trick == 'trick_3':
+        itemset = trick_3(counter, k, itemset)
+
     logger.info(f' get l_k len:{len(l_k)} ')
     return l_k, sp_s, itemset
+
+
+def count_freq(itemset):
+    """ Count freq of item in itemset
+    itemset: dataset
+    """
+    counter = {}
+    for item in tqdm(itemset):
+        for record in item:
+            if record in counter:
+                counter[record] += 1
+            else:
+                counter[record] = 1
+
+    return counter
+
+
+def find_frequent_patterns(itemset, support):
+    dataset = []
+    support = support * len(itemset)
+    for item in tqdm(itemset):
+        dataset.append(item.keys())
+    return pyfpgrowth.find_frequent_patterns(dataset, support)
+
+
+def generate_association_rules(patterns, rate):
+    return pyfpgrowth.generate_association_rules(patterns, rate)
+
+
+def calculate_association_rules(all_lks, all_sps, confidence):
+    counter = {}
+
+    # Step1. 将 list to map
+    for lk, sp in tqdm(zip(all_lks, all_sps)):
+        counter[tuple(sorted(lk))] = sp
+    rules = []
+    filters = {}
+    for lk in tqdm(all_lks):
+        # print(type(lk))
+        for i in range(1, len(lk)):
+            combines = list(combinations(lk, i))
+            for a in combines:
+                a_s = tuple(sorted(a))
+                b = list(set(lk).difference(set(a)))
+                b_s = tuple(sorted(b))
+                key = tuple(sorted(lk))
+                max_info = None
+                max_confidence = 0
+                if a_s in counter and (counter[key] / counter[a_s]) > confidence and (counter[key] / counter[a_s]) > max_confidence:
+                    info = f'{a_s}:{b_s}, {(counter[key] / counter[a_s])}'
+                    max_confidence = (counter[key] / counter[a_s])
+                    if info not in filters:
+                        max_info = info
+                        filters[info] = 1
+                if b_s in counter and (counter[key] / counter[b_s]) > confidence and (counter[key] / counter[b_s]) > max_confidence:
+                    info = f'{b_s}:{a_s}, { (counter[key] / counter[b_s])}'
+                    max_confidence = (counter[key] / counter[b_s])
+                    if info not in filters:
+                        max_info = info
+                        filters[info] = 1
+                if max_info is not None:
+                    rules.append(max_info)
+    return rules
+
+
+if __name__ == '__main__':
+    rules = calculate_association_rules([['1', '2', '3'], ['2', '3'], ['4'], ['5']],
+                                        [3, 5, 4, 5],
+                                        0.03)
+    print(rules)
